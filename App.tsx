c12a25bd07e75dd493a625e6ac6c85a9e88bd7f2
tallboy <yourspaceworkteam@gmail.com>
2022-06-14 11:27:16 +0300

    edit branch

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View, ActivityIndicator} from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
 import WeatherApp from './src/screens/WeatherApp';
 
 export default function App() {
     return (
         <View>
-            <WeatherApp/>
+            <WeatherApp />
         </View>
     );
 }
